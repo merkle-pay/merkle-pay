56182bcdcce3ebccb0a168990b35ef34d44b20da
Yumin Gui <guiyumin@protonmail.com>
2025-03-14 21:11:42 -0700

    fix: some basic stuff

---
+++ utils/prices.ts
@@ -0,0 +1,19 @@
+// {
+//     "id": "7fb769c6-f723-44d4-ba09-bd3b117742b3",
+//     "success": true,
+//     "data": {
+//       "So11111111111111111111111111111111111111112": "239.02618235836016"
+//     }
+// }
+// "https://api-v3.raydium.io/mint/price?mints=So11111111111111111111111111111111111111112"
+
+// sol price in USDT
+export const solRealTimePrice = async (): Promise<number | null> => {
+  const response = await fetch(
+    "https://api-v3.raydium.io/mint/price?mints=So11111111111111111111111111111111111111112"
+  );
+  const json = await response.json();
+  return json.success
+    ? json.data["So11111111111111111111111111111111111111112"]
+    : null;
+};
