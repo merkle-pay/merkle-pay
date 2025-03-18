430c5f4fc7eca246e8958d3f3b83520b7de1f74c
Yumin Gui <guiyumin@protonmail.com>
2025-03-18 00:33:17 -0700

    fix: merkle pay text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,12 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Solana Pay</h1>
+    <div className="flex flex-col">
+      <h1>Merkle Pay</h1>
       <p>
-        Solana Pay is a payment protocol for the Solana blockchain. It allows
-        users to send and receive payments using the Solana blockchain.
+        Merkle Pay is a payment protocol for the blockchain. It allows users to
+        send and receive payments using stablecoins, such as USDC and USDT.
       </p>
+      <button>Pay</button>
     </div>
   );
 }
