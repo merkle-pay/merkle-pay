c9f779423f4dc59fc958f5b8f1892c9ba6b4165a
Yumin Gui <guiyumin@protonmail.com>
2025-04-14 12:20:36 -0700

    fix: update base path in Vite configuration

--- apps/merkle-dashboard/vite.config.ts
+++ apps/merkle-dashboard/vite.config.ts
@@ -7,7 +7,7 @@ import svgr from 'vite-plugin-svgr'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), TanStackRouterVite(), svgr()],
-  base: '/dashboard/',
+  base: '/dashboard',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
