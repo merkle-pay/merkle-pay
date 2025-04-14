8894a0e96933a629c0bda71b64dd20ff147efd58
Yumin Gui <guiyumin@protonmail.com>
2025-04-14 12:14:34 -0700

    feat: add svgr plugin and update a devtool lib

--- apps/merkle-dashboard/vite.config.ts
+++ apps/merkle-dashboard/vite.config.ts
@@ -2,10 +2,11 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [react(), TanStackRouterVite(), svgr()],
   base: '/dashboard/',
   resolve: {
     alias: {
