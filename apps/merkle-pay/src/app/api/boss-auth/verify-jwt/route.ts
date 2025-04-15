533282190f914e2bf28a4488165564dc9d76c846
Yumin Gui <guiyumin@protonmail.com>
2025-04-14 22:23:38 -0700

    fix: handle JWT verification errors

--- apps/merkle-pay/src/app/api/boss-auth/verify-jwt/route.ts
+++ apps/merkle-pay/src/app/api/boss-auth/verify-jwt/route.ts
@@ -4,17 +4,25 @@ import { auth as bossAuth } from "src/utils/boss-auth";
 import { jwtVerify, createLocalJWKSet } from "jose";
 
 export async function POST(request: NextRequest) {
-  const jwks = await bossAuth.api.getJwks();
+  try {
+    const jwks = await bossAuth.api.getJwks();
 
-  const JWKS = createLocalJWKSet({
-    keys: jwks.keys,
-  });
+    const JWKS = createLocalJWKSet({
+      keys: jwks.keys,
+    });
 
-  const { jwt } = await request.json();
+    const { jwt } = await request.json();
 
-  const { payload } = await jwtVerify(jwt, JWKS);
+    await jwtVerify(jwt, JWKS);
+
+    return NextResponse.json({
+      success: true,
+    });
+  } catch (error) {
+    console.error(error);
+  }
 
   return NextResponse.json({
-    payload,
+    success: false,
   });
 }
