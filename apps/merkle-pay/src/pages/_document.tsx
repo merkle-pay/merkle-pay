c650aa0d6f0619232c5dcbfc42171af535942cd0
Yumin Gui <guiyumin@protonmail.com>
2025-04-18 15:48:42 -0700

    feat: add Turnstile site key integration in document head

--- apps/merkle-pay/src/pages/_document.tsx
+++ apps/merkle-pay/src/pages/_document.tsx
@@ -1,9 +1,18 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  const turnstileSiteKey = process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY;
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {turnstileSiteKey && (
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.CF_TURNSTILE_SITE_KEY = "${turnstileSiteKey}";`,
+            }}
+          />
+        )}
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
