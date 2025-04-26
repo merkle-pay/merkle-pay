d97fa31c1d7fd7f34a438368d237fd469aba8266
Yumin Gui <guiyumin@protonmail.com>
2025-04-25 19:47:33 -0700

    feat: phantom deeplink, part 12

--- apps/merkle-pay/src/components/cf-turnstile/index.tsx
+++ apps/merkle-pay/src/components/cf-turnstile/index.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useRef, useImperativeHandle, Ref } from "react";
+import React, { useRef, useImperativeHandle, Ref } from "react";
 import {
   Turnstile,
   TurnstileInstance,
@@ -14,34 +14,31 @@ export type CfTurnstileProps = {
   siteKey: string;
   setToken?: (token: string) => void;
   options?: TurnstileProps["options"];
+  ref: Ref<CfTurnstileHandle>;
 };
 
-export const CfTurnstile = forwardRef<CfTurnstileHandle, CfTurnstileProps>(
-  (props, ref: Ref<CfTurnstileHandle>) => {
-    const { siteKey, setToken, options } = props;
-    const innerRef = useRef<TurnstileInstance>(null);
+export const CfTurnstile = (props: CfTurnstileProps) => {
+  const { siteKey, setToken, options, ref } = props;
+  const innerRef = useRef<TurnstileInstance>(null);
 
-    useImperativeHandle(
-      ref,
-      () => ({
-        reset: () => innerRef.current?.reset(),
-        getResponseAsync: () => innerRef.current?.getResponsePromise(),
-      }),
-      []
-    );
+  useImperativeHandle(
+    ref,
+    () => ({
+      reset: () => innerRef.current?.reset(),
+      getResponseAsync: () => innerRef.current?.getResponsePromise(),
+    }),
+    []
+  );
 
-    return (
-      <Turnstile
-        ref={innerRef}
-        siteKey={siteKey}
-        options={options}
-        onSuccess={(token) => setToken?.(token)}
-        onError={() => innerRef.current?.reset()}
-        onExpire={() => innerRef.current?.reset()}
-        onTimeout={() => innerRef.current?.reset()}
-      />
-    );
-  }
-);
-
-CfTurnstile.displayName = "CfTurnstile";
+  return (
+    <Turnstile
+      ref={innerRef}
+      siteKey={siteKey}
+      options={options}
+      onSuccess={(token) => setToken?.(token)}
+      onError={() => innerRef.current?.reset()}
+      onExpire={() => innerRef.current?.reset()}
+      onTimeout={() => innerRef.current?.reset()}
+    />
+  );
+};
