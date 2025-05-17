64676ccd7c6f1e6d0b65aac29a293793f070cc89
Yumin Gui <guiyumin@protonmail.com>
2025-05-17 00:00:08 -0700

    feat: update GetTxStatus response schema and handle blockchain-specific logic

--- apps/merkle-server/src/public-routes/solana.ts
+++ apps/merkle-server/src/public-routes/solana.ts
@@ -10,8 +10,12 @@ type GetTxStatusQuerystring = Static<typeof GetTxStatusQuerystringSchema>;
 
 // Define the schema for the response
 const GetTxStatusResponseSchema = Type.Object({
-  status: Type.String(),
-  confirmationTime: Type.Optional(Type.Number()),
+  code: Type.Number(),
+  message: Type.String(),
+  data: Type.Object({
+    status: Type.String(),
+    txHash: Type.Optional(Type.String()),
+  }).or(Type.Null()),
 });
 // Infer the static type for the response
 type GetTxStatusResponse = Static<typeof GetTxStatusResponseSchema>;
@@ -42,12 +46,40 @@ export async function routes(
           [mpid]
         );
 
-        console.log("result", result.rows);
+        if (result.rowCount !== 1) {
+          return reply.status(200).send({
+            code: 404,
+            message: "not found",
+            data: {
+              status: "not found",
+            },
+          });
+        }
+
+        const payment = result.rows[0];
+
+        if (payment.blockchain === "solana") {
+          // fetch tx status from solana
+        }
+
+        if (payment.blockchain === "tron") {
+          // fetch tx status from tron
+        }
+
+        return reply.status(200).send({
+          code: 200,
+          message: "Blockchain not supported",
+          data: null,
+        });
       } catch (error) {
         console.error("error", error);
       }
 
-      return { status: "confirmed" };
+      return reply.status(200).send({
+        code: 500,
+        message: "Internal server error",
+        data: null,
+      });
     }
   );
 }
