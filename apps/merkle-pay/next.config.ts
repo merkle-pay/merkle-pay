db198e1ebe4377c64a95149a506bd2442df05cc1
Yumin Gui <guiyumin@protonmail.com>
2025-05-22 21:24:32 -0700

    fix: seems no need to use next redirect

--- apps/merkle-pay/next.config.ts
+++ apps/merkle-pay/next.config.ts
@@ -3,14 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/dashboard",
-        destination: "/dashboard/index.html",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
